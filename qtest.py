2cd586772b12f4aaa16693d9d397dd770363ead6
Michał Sidor <michcioperz@gmail.com>
2015-10-06 19:53:27 +0200

    couldn't have been simpler could it

---
+++ qtest.py
@@ -0,0 +1,11 @@
+import subprocess, os, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", type=str)
+args = parser.parse_args()
+
+subprocess.check_call(["g++",'-o','./qtest.out',os.path.join(args.directory,args.directory+".cpp")])
+
+for i in sorted([x for x in os.listdir(args.directory) if x.endswith(".in")]):
+    with open(os.path.join(args.directory, i)) as f, open(os.path.join(args.directory, i[:-2]+"out")) as g:
+        print(i+" "+str(subprocess.check_output(["./qtest.out"], universal_newlines=True, input=f.read()).strip() == g.read().strip()))
