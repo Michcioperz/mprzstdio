00af1c39adc5ea33b440162cd3183db06f70654d
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-01 21:58:51 +0200

    you can now make all tests feel special

--- qtest.py
+++ qtest.py
@@ -4,9 +4,10 @@ import subprocess, os, argparse, re
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", type=str)
 parser.add_argument("-s", "--special", type=str, action='append')
+parser.add_argument("-v", "--verbose", action="store_true")
 args = parser.parse_args()
 
-subprocess.check_call(["g++",'-o','./.qtest.out',os.path.join(args.directory,args.directory+".cpp")])
+subprocess.check_call(["g++",'-g','-o','./.qtest.out',os.path.join(args.directory,args.directory+".cpp")])
 
 results = {}
 def count(x):
@@ -15,7 +16,7 @@ def count(x):
 
 for i in sorted([x for x in os.listdir(args.directory) if x.endswith(".in")], key=lambda x: int(re.findall(r"\d+",x)[0])):
     with open(os.path.join(args.directory, i)) as f, open(os.path.join(args.directory, i[:-2]+"out")) as g:
-        if args.special and i in args.special: print("Now we'll debug %s" % i)
-        print(i+" "+str(count(subprocess.check_output(["./.qtest.out"], universal_newlines=True, stderr=(None if args.special and i in args.special else subprocess.DEVNULL), input=f.read()).strip() == g.read().strip())))
+        if args.verbose or args.special and i in args.special: print("Now we'll debug %s" % i)
+        print(i+" "+str(count(subprocess.check_output(["./.qtest.out"], universal_newlines=True, stderr=(None if args.verbose or args.special and i in args.special else subprocess.DEVNULL), input=f.read()).strip() == g.read().strip())))
 
 print(results)
