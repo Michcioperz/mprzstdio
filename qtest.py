2cc34d48ffc5c52318808ca3a84026cfc62e6e49
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-01 20:00:46 +0200

    better sorting, final results summary, special treatment for problematic tests
    also temporary executable was renamed and git-ignored

--- qtest.py
+++ qtest.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
-import subprocess, os, argparse
+import subprocess, os, argparse, re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", type=str)
+parser.add_argument("-s", "--special", type=str, action='append')
 args = parser.parse_args()
 
-subprocess.check_call(["g++",'-o','./qtest.out',os.path.join(args.directory,args.directory+".cpp")])
+subprocess.check_call(["g++",'-o','./.qtest.out',os.path.join(args.directory,args.directory+".cpp")])
 
-for i in sorted([x for x in os.listdir(args.directory) if x.endswith(".in")]):
+results = {}
+def count(x):
+    results[x] = results.get(x, 0) + 1
+    return x
+
+for i in sorted([x for x in os.listdir(args.directory) if x.endswith(".in")], key=lambda x: int(re.findall(r"\d+",x)[0])):
     with open(os.path.join(args.directory, i)) as f, open(os.path.join(args.directory, i[:-2]+"out")) as g:
-        print(i+" "+str(subprocess.check_output(["./qtest.out"], universal_newlines=True, input=f.read()).strip() == g.read().strip()))
+        if args.special and i in args.special: print("Now we'll debug %s" % i)
+        print(i+" "+str(count(subprocess.check_output(["./.qtest.out"], universal_newlines=True, stderr=(None if args.special and i in args.special else subprocess.DEVNULL), input=f.read()).strip() == g.read().strip())))
+
+print(results)
